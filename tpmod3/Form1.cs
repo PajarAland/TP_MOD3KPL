bb9e4335a0bc32bd08e2e900491714d131a54793
unknown <raymonto08@gmail.com>
2024-03-04 00:09:33 +0700

    First Commit

---
+++ tpmod3/Form1.cs
@@ -0,0 +1,35 @@
+namespace tpmod3;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userInput = textBox1.Text;
+            label1.Text = "Halo! " + userInput;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string userInput = textBox1.Text;
+            Console.WriteLine(userInput);
+        }
+    }
+
